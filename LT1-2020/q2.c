86c5a599c06f7885af71806ce25ada5916f94f54
Wen Liang <wenlianggg@gmail.com>
2020-12-07 10:41:57 +0800

    Reorganise LT1

---
+++ LT1-2020/q2.c
@@ -0,0 +1,91 @@
+/*
+ * Name : Wen Liang
+ * Email: wlgoh.2020
+ */
+
+#include <stdio.h>
+
+// write your answer below
+void move_robot(int position[], char moves[], int n) {
+    int num_moves = 1;
+    for (int i = 0; i < n; i++) {
+        int move = moves[i];
+        if (move >= '0' && move <= '9') {
+            num_moves = move - '0';
+        }
+        switch(move) {
+            case 'N':
+                position[1] += num_moves;
+                num_moves = 1;
+                break;
+            case 'S':
+                position[1] -= num_moves;
+                num_moves = 1;
+                break;
+            case 'E':
+                position[0] += num_moves;
+                num_moves = 1;
+                break;
+            case 'W':
+                position[0] -= num_moves;
+                num_moves = 1;
+                break;
+        }
+    }
+}
+
+// do not edit the main method.
+int main_q2(void) {
+    int tc_num = 1;
+    {
+        printf("Test %d\n", tc_num++);
+        char moves[] = {'N', 'N', 'N', 'S', 'S'};
+        int position[] = {0, 0};
+        move_robot(position, moves, sizeof(moves) / sizeof(char));
+        printf("Expected:(0,1)\n");
+        printf("Actual  :(%d,%d)\n", position[0], position[1]);
+        printf("\n");
+    }
+
+    {
+        printf("Test %d\n", tc_num++);
+        char moves[] = {'N', 'E', 'W', 'S', 'N', 'E', 'W', 'S'};
+        int position[] = {1, 1};
+        move_robot(position, moves, sizeof(moves) / sizeof(char));
+        printf("Expected:(1,1)\n");
+        printf("Actual  :(%d,%d)\n", position[0], position[1]);
+        printf("\n");
+    }
+
+    {
+        printf("Test %d\n", tc_num++);
+        char moves[] = {'5', 'N', 'E', 'E'};
+        int position[] = {3, 1};
+        move_robot(position, moves, sizeof(moves) / sizeof(char));
+        printf("Expected:(5,6)\n");
+        printf("Actual  :(%d,%d)\n", position[0], position[1]);
+        printf("\n");
+    }
+
+    {
+        char moves[] = {'N', 'N', 'N', 'S', 'S'};
+        int position[] = {0, 0};
+        move_robot(position, moves, sizeof(moves) / sizeof(char));
+        printf("Test %d\n", tc_num++);
+        printf("Expected:(0,1)\n");
+        printf("Actual  :(%d,%d)\n", position[0], position[1]);
+        printf("\n");
+    }
+
+    {
+        char moves[] = {'3', 'N', 'S', 'E', 'E', '2', 'E'};
+        int position[] = {1, 5};
+        move_robot(position, moves, sizeof(moves) / sizeof(char));
+        printf("Test %d\n", tc_num++);
+        printf("Expected:(5,7)\n");
+        printf("Actual  :(%d,%d)\n", position[0], position[1]);
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ LT1-2020/q6.c
@@ -0,0 +1,65 @@
+/*
+You must NOT modify this file.
+*/
+
+#include <stdio.h>
+#include "all_headers.h"
+
+int main_q6(void) {
+    int tc_num = 1;
+    {
+        int timings[] = {
+            600, 700,
+            700, 800,
+            800, 900};
+
+        int result = get_crowd_size(timings, sizeof(timings) / sizeof(int));
+        printf("Test %d\n", tc_num++);
+        printf("Expected:1\n");
+        printf("Actual  :%d\n", result);
+        printf("\n");
+    }
+
+    {
+        int timings[] = {600, 900,
+                         700, 900,
+                         800, 900};
+
+        int result = get_crowd_size(timings, sizeof(timings) / sizeof(int));
+        printf("Test %d\n", tc_num++);
+        printf("Expected:3\n");
+        printf("Actual  :%d\n", result);
+        printf("\n");
+    }
+
+        {
+        int timings[] = {
+            600, 1300,
+            700, 1400,
+            1400, 1500,
+            1400, 1600,
+            1700, 2200};
+
+        int result = get_crowd_size(timings, sizeof(timings) / sizeof(int));
+        printf("Test %d\n", tc_num++);
+        printf("Expected:2\n");
+        printf("Actual  :%d\n", result);
+        printf("\n");
+    }
+
+        {
+        int timings[] = {
+            600, 900,
+            700, 800,
+            730, 830,
+            800, 1200};
+
+        int result = get_crowd_size(timings, sizeof(timings) / sizeof(int));
+        printf("Test %d\n", tc_num++);
+        printf("Expected:3\n");
+        printf("Actual  :%d\n", result);
+        printf("\n");
+    }
+
+    return 0;
+}
